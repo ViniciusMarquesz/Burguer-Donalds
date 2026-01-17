e61636faa81b6fc14314d228f04a90b4cf488a49
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2026-01-17 14:15:49 -0300

    feat: ensure order can be created

--- src/app/[slug]/menu/components/finish-order-dialog.tsx
+++ src/app/[slug]/menu/components/finish-order-dialog.tsx
@@ -2,8 +2,12 @@
 
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ConsumptionMethod } from "@prisma/client";
+import { useParams, useSearchParams } from "next/navigation";
+import { useContext, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import {PatternFormat} from 'react-number-format'; 
+import { toast } from "sonner";
 import {z} from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -20,7 +24,10 @@ import {
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+import { createOrder } from "../actions/create-order";
+import { CartContext } from "../contexts/cart";
 import { isValidCpf } from "../helpers/cpf";
+import { Loader2Icon } from "lucide-react";
 
 const formSchema = z.object({
   name: z.string().trim().min(1, {
@@ -46,6 +53,11 @@ interface FinishOrderDialogProps {
 
   const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
 
+    const [isPending, startTransition] = useTransition()
+    const { slug } = useParams<{slug: string}>();
+    const searchParams = useSearchParams()
+    const { products } = useContext(CartContext)
+
     const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -55,8 +67,28 @@ interface FinishOrderDialogProps {
     shouldUnregister: true,
   });
 
-  const onSubmit = (data: FormSchema) => {
-    console.log({data});
+  const onSubmit = async (data: FormSchema) => {
+    try {
+
+      const consumptionMethod = searchParams.get("consumptionMethod") as ConsumptionMethod;
+
+      startTransition(async () => {
+         await createOrder({
+          consumptionMethod,
+          customerCpf: data.cpf,
+          customerName: data.name,
+          products,
+          slug,
+        });
+        onOpenChange(false)
+        toast.success("Pedido finalizado com sucesso.")    
+      })
+
+      
+          
+    } catch (error) {
+      console.error(error);
+    }
   }
 
 return ( 
@@ -106,7 +138,14 @@ return (
         />
 
           <DrawerFooter>
-            <Button type="submit" variant="destructive" className="rounded-full">Finalizar</Button>
+            <Button 
+            type="submit" 
+            variant="destructive" 
+            className="rounded-full"
+            disabled={isPending}
+            >
+              {isPending && <Loader2Icon className="animate-spin" />}
+              Finalizar</Button>
             <DrawerClose asChild>
               <Button variant="secondary" className="w-full rounded-full">Cancelar</Button>
             </DrawerClose>
